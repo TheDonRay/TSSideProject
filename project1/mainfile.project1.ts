d71816270d0c9b10f56e7a4de3e8770cc3073547
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-29 19:26:57 -0400

    working on this typescript project here

--- project1/mainfile.project1.ts
+++ project1/mainfile.project1.ts
@@ -14,7 +14,6 @@ interface UserLevel {
 }; 
 
 
-
 class UserProfile {
     private user: User;
     protected password: UserPassword; 
@@ -27,6 +26,9 @@ class UserProfile {
         this.level = level; 
     };  
 
+   public userNames = () => { 
+    return 
+   }
     // create this function to get users  username as such 
     public getUsername = () => { 
         return `User's Username is ${this.user.username}`; 
@@ -39,5 +41,24 @@ class UserProfile {
     public getUserPassword = () => { 
         return `Sorry User password is confidential cannot show`; 
     }
+};  
+
+// create a object instance for the class here 
+const user1 = new UserProfile( 
+    {name: 'Ray', age: 20, username: 'thedevray'}, 
+    {password: '2005gamerboyray'}, 
+    {level: 1}, 
+); 
+
+console.log(user1.getUsername());  
+
+
+interface Game { 
+    gameName: string
 }; 
 
+const game : Game = { gameName : 'Killer3' }; 
+
+console.log(game); //  prints the whole object: { gameName: 'Killer3' }
+
+console.log(game.gameName); // property access (dot notation).
