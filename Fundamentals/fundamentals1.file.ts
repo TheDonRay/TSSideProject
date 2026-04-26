fe52dde6ac1ffec21579d1f7677dbfe6dd15f53e
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-04-25 23:55:18 -0400

    working on the fundamentals here

--- Fundamentals/fundamentals1.file.ts
+++ Fundamentals/fundamentals1.file.ts
@@ -1,30 +1,71 @@
-// lets say we have this object here which can have any type. 
+// // lets say we have this object here which can have any type. 
 
-// so a interface you may see here as such 
-interface Recorder { 
-    name: string, 
-    age: number, 
+// // so a interface you may see here as such 
+// interface Recorder { 
+//     name: string, 
+//     age: number, 
+// }; 
+
+// const data : Recorder = { name: 'ray', age: 21}; 
+
+// console.log(data);  
+
+// // Function Parameter Type annotations here as such 
+// //example functions here 
+// function greeting(name: string){ 
+//     console.log('Hello, ' + name.toUpperCase() + '!'); 
+// }; 
+
+// // so now if we were to invoke this function here as such 
+// greeting('ray');  
+
+// // you can have return type Annotations here as such too 
+// function NumValue() : number { 
+//     return 21; 
+// } 
+
+// // another notation that  you can have is such 
+// const exampleFunction = () : number => { 
+//     return 3; 
+// } 
+
+// function call here as such 
+
+// Functions with return promises as such  
+// async function AnotherExampleAsync() : Promise<number> { 
+//     return 21+32; 
+// }; 
+
+// const result = await AnotherExampleAsync();
+// console.log(result); 
+
+//Understanding Object Type 
+
+function exampleFunction(pt : {x: number, y: string}) { 
+    console.log('The value of the X key is', pt.x); 
+    console.log('The value of the Y key is', pt.y); 
 }; 
 
-const data : Recorder = { name: 'ray', age: 21}; 
+// invoke this function with values 
+exampleFunction({x: 21, y: 'Ray'});  
 
-console.log(data);  
-
-// Function Parameter Type annotations here as such 
-//example functions here 
-function greeting(name: string){ 
-    console.log('Hello, ' + name.toUpperCase() + '!'); 
+// now when it comes to object properties some values may actually be optional. To represent this we can simply apply the ? mark symbol.  
+function ObjectOptional(dataContent: {content: string, data?: string, isValid: boolean}) { 
+    // handle some logic here as such  
+    try { 
+        if (dataContent.content === 'valid') { 
+            return true; 
+        } else { 
+            console.log('content is not true'); 
+        }
+    } catch (error) { 
+        console.log(error); 
+        return false; 
+    }
 }; 
 
-// so now if we were to invoke this function here as such 
-greeting('ray');  
+// As you can see here the data is valid here where we can all three fields which is fine 
+ObjectOptional({content: 'valid', data: 'Secret Data', isValid: true}); 
+// we can also have the function call as this as well, as you can see both are okay so it doesn't matter 
+ObjectOptional({content: 'valid', isValid: false});  
 
-// you can have return type Annotations here as such too 
-function NumValue() : number { 
-    return 21; 
-} 
-
-// another notation that  you can have is such 
-const exampleFunction = () : number => { 
-    return 3; 
-}
